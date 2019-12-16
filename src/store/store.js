88df70ebdf165e9b4d97fb80627a7365350d21d6
Mihail Romaniuk <romanyuk.mike@gmail.com>
2019-12-16 07:53:27 +0200

    removed redux logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 
 export const configureStore = reducers => (
   createStore(
     reducers,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   )
 );
